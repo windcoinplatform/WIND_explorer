7d8cbfa0f88565e3356b48d201f2632d0ac41043
beregovoy68 <s.a.ishchenko@gmail.com>
2017-10-29 23:17:27 +1100

    Installed material and router modules. Added required dependencies to app.

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,11 @@ const config = {
     libraries: {
         sources: [
             'node_modules/angular/angular.js',
+            'node_modules/angular-route/angular-route.js',
+            'node_modules/angular-animate/angular-animate.js',
+            'node_modules/angular-aria/angular-aria.js',
+            'node_modules/angular-messages/angular-messages.js',
+            'node_modules/angular-material/angular-material.js',
             'node_modules/angular-loading-bar/build/loading-bar.js',
             'node_modules/@uirouter/angularjs/release/angular-ui-router.js',
             'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
