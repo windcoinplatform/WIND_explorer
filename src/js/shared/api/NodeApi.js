b54c1b86dfd2cea2b0d9dfecfd0bf0bb3600ba77
beregovoy68 <s.a.ishchenko@gmail.com>
2019-07-15 22:58:12 +1000

    EXP-490: Changed library to implement retries

--- src/js/shared/api/NodeApi.js
+++ src/js/shared/api/NodeApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import * as rax from 'retry-axios';
+import axiosRetry, {exponentialDelay} from 'axios-retry';
 import json from 'json-bigint';
 
 import DateTime from '../DateTime';
@@ -41,14 +41,6 @@ const buildAxiosConfig = useCustomRequestConfig => {
     return result;
 };
 
-const buildRetryableAxiosConfig = axiosInstance => ({
-    instance: axiosInstance,
-    retryDelay: 100,
-    retry: 5,
-    httpMethodsToRetry: ['GET'],
-    shouldRetry: shouldRetryRequest
-});
-
 export const replaceTimestampWithDateTime = obj => {
     if (obj.timestamp) {
         obj.timestamp = new DateTime(obj.timestamp);
@@ -67,68 +59,16 @@ const transformTimestampToDateTime = (responseData) => {
     return responseData;
 };
 
-/**
- * Determine based on config if we should retry the request.
- * @param err The AxiosError passed to the interceptor.
- */
-function shouldRetryRequest(err) {
-    const config = err.config.raxConfig;
-
-    // If there's no config, or retries are disabled, return.
-    if (!config || config.retry === 0) {
-        return false;
-    }
-
-    config.currentRetryAttempt = config.currentRetryAttempt || 0;
-
-    // Check if this error has no response (ETIMEDOUT, ENOTFOUND, etc)
-    if (!err.response && (config.currentRetryAttempt >= config.noResponseRetries)) {
-        return false;
-    }
-
-    // Only retry with configured HttpMethods.
-    const methodsToRetry = Array.isArray(config.httpMethodsToRetry) ?
-        config.httpMethodsToRetry :
-        Object.values(config.httpMethodsToRetry);
-    if (!err.config.method ||
-        methodsToRetry.indexOf(err.config.method.toUpperCase()) < 0) {
-        return false;
-    }
-
-    // If this wasn't in the list of status codes where we want
-    // to automatically retry, return.
-    if (err.response && err.response.status) {
-        let isInRange = false;
-        for (const [min, max] of config.statusCodesToRetry) {
-            const status = err.response.status;
-            if (status >= min && status <= max) {
-                isInRange = true;
-                break;
-            }
-        }
-        if (!isInRange) {
-            return false;
-        }
-    }
-
-    // If we are out of retry attempts, return
-    if (config.currentRetryAttempt >= config.retry) {
-        return false;
-    }
-
-    return true;
-}
-
 export const nodeApi = (baseUrl, useCustomRequestConfig) => {
     const trimmedUrl = Strings.trimEnd(baseUrl, '/');
     const config = buildAxiosConfig(useCustomRequestConfig);
     const nodeAxios = axios.create(config);
-    const get = (url, config) => nodeAxios.get(trimmedUrl + url, config);
+    axiosRetry(nodeAxios, {
+        retries: 5,
+        retryDelay: exponentialDelay
+    });
 
-    const retryableAxios = axios.create(config);
-    retryableAxios.defaults.raxConfig = buildRetryableAxiosConfig(retryableAxios);
-    rax.attach(retryableAxios);
-    const retryableGet = (url, config) => retryableAxios.get(trimmedUrl + url, config);
+    const get = (url, config) => nodeAxios.get(trimmedUrl + url, config);
 
     return {
         version: () => get('/node/version'),
@@ -159,7 +99,7 @@ export const nodeApi = (baseUrl, useCustomRequestConfig) => {
         transactions: {
             unconfirmed: () => get('/transactions/unconfirmed'),
             utxSize: () => get('/transactions/unconfirmed/size'),
-            info: id => retryableGet(`/transactions/info/${id}`),
+            info: id => get(`/transactions/info/${id}`),
             address: (address, limit, after) => {
                 const top = limit || TRANSACTIONS_BY_ADDRESS_LIMIT;
                 const config = after ? {
