3566c84a37f8d001d8db4172beab1dbaf48a2e25
windcoinplatform <57537825+windcoinplatform@users.noreply.github.com>
2019-11-08 22:52:50 +0300

    Update ConfigurationForm.js

--- src/js/ConfigurationForm.js
+++ src/js/ConfigurationForm.js
@@ -97,7 +97,7 @@ export default class ConfigurationForm extends React.Component {
 
                         <div className="row">
                             <label>Node address</label>
-                            apiBaseUrl: "http://144.91.84.27:6869">
+                            apiBaseUrl: "http://144.91.84.27:6869"
                         </div>
 
                         <div className="row buttons-wrapper">
