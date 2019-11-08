1493cc55cb4b5ccd352197322766d8b99072068c
windcoinplatform <57537825+windcoinplatform@users.noreply.github.com>
2019-11-08 22:46:19 +0300

    Update index.js

--- src/js/configuration/index.js
+++ src/js/configuration/index.js
@@ -1,8 +1,8 @@
 import mainnet from './config.mainnet';
-import custom from './config.custom';
 
 
-const configuredNetworks = [mainnet, custom]
+
+const configuredNetworks = [mainnet]
     .filter(item => __NETWORKS__.includes(item.networkId));
 
 export default configuredNetworks;
