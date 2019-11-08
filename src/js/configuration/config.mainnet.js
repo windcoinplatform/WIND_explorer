5fc3bae8ee6835fe53bf95d91d341e544b02f97c
windcoinplatform <57537825+windcoinplatform@users.noreply.github.com>
2019-11-08 22:33:49 +0300

    Update config.mainnet.js

--- src/js/configuration/config.mainnet.js
+++ src/js/configuration/config.mainnet.js
@@ -1,11 +1,10 @@
-const nodeUrl = 'https://nodes.wavesnodes.com';
+const nodeUrl = 'http://144.91.84.27:6869';
 
 export default {
     networkId: 'mainnet',
     displayName: 'Mainnet',
-    apiBaseUrl: nodeUrl,
+    apiBaseUrl: 'http://144.91.84.27:6869',
     useCustomRequestConfig: true,
-    dataServicesBaseUrl: 'https://api.wavesplatform.com/v0',
-    spamListUrl: 'https://raw.githubusercontent.com/wavesplatform/waves-community/master/Scam%20tokens%20according%20to%20the%20opinion%20of%20Waves%20Community.csv',
+   
     nodes: [{url: nodeUrl, maintainer: 'Waves'}]
 };
