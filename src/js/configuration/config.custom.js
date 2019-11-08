39004627c0b8bb9efd4f0eef04f330e45541d49f
windcoinplatform <57537825+windcoinplatform@users.noreply.github.com>
2019-11-08 22:46:34 +0300

    Delete config.custom.js

--- src/js/configuration/config.custom.js
+++
@@ -1,8 +0,0 @@
-const nodeUrl = 'http://144.91.84.27:6869';
-
-export default {
-    networkId: 'custom',
-    displayName: 'Custom',
-    apiBaseUrl: 'http://144.91.84.27:6869',
-    useCustomRequestConfig: true,
-
