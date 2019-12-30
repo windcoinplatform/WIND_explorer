691a76eb2c39f09255128030fbba330dd171f3d5
WIND <57537825+windcoinplatform@users.noreply.github.com>
2019-12-30 21:27:49 +0300

    Update Footer.js

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -30,7 +30,7 @@ const Footer = ({version}) => {
                     (<a key={item.id} className={`social ${item.id}`} href={item.url} target="_blank"></a>))}
             </div>
             <div>
-                <a className="fade" href="http://173.212.251.111:60863" target="_blank">WINDWALLET</a>
+                <a className="fade" href="http://144.91.84.27/:60863" target="_blank">WINDWALLET</a>
             </div>
         </div>
     );
