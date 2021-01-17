9155a519ad822f6c136eff9deebe2b58f407a12b
WIND <57537825+windcoinplatform@users.noreply.github.com>
2021-01-17 22:05:39 +0300

    Update config.mainnet.js

--- src/js/configuration/config.mainnet.js
+++ src/js/configuration/config.mainnet.js
@@ -3,7 +3,7 @@ const nodeUrl = 'http://161.97.178.27:6869';
 export default {
     networkId: 'mainnet',
     displayName: 'Mainnet',
-    apiBaseUrl: 'http://144.91.84.27:6869',
+    apiBaseUrl: 'http://161.97.178.277:6869',
     useCustomRequestConfig: false,
    
     nodes: [{url: nodeUrl, maintainer: 'Wind'}]
